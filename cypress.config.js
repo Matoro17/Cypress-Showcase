89e311a84b982acc23109d73982fef774b8d1533
Gabriel Silva de Azevedo <gabrielsilvadeazevedo@gmail.com>
2022-07-29 10:52:49 -0300

    updated all project structure

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'whcbur',
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
+    experimentalSessionAndOrigin: true,
+  },
+  reporterOptions: {
+    reportFilename: "[status]_[datetime]-[name]-report",
+    quiet: true,
+    overwrite: false,
   },
 });
