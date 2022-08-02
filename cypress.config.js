92ad5b057df52b06831499aa6755094baeaae297
Gabriel Silva de Azevedo <gabrielsilvadeazevedo@gmail.com>
2022-08-02 19:52:55 -0300

    altered some configs

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
-    experimentalSessionAndOrigin: true,
   },
   reporterOptions: {
     reportFilename: "[status]_[datetime]-[name]-report",
